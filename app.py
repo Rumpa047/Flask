96551bca45af6a00aa3981cf601991737813b5ac
Rumpa047 <47315020+Rumpa047@users.noreply.github.com>
2019-02-04 15:58:54 +0600

    3rd commit
    add login.html and login router. input  name,  load login url.

--- app.py
+++ app.py
@@ -1,27 +1,37 @@
-from flask import Flask
+from flask import Flask, redirect, request, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-
     return 'Welcome to home page.'
 
 @app.route('/hello')
 def hello_world():
-
     return 'Hello World.'
 
 @app.route('/welcome')
 def welcome():
-
     return 'Welcome to new page.'
 
 @app.route('/hello/<name>')
 def hello_name(name):
-
     return 'Welcome %s.' %name
+
+@app.route('/success/<name>')
+def success(name):
+    return 'Hi %s.' %name
+
+@app.route('/login', methods=['POST' , 'GET'])
+def login():
+    if request.method == 'POST':
+        user = request.form['nm']
+        return redirect(url_for('success', name = user))
+
+    else:
+        user = request.args.get('nm')
+        return redirect(url_for('success', name = user))
 
 if __name__ == "__main__":
 
